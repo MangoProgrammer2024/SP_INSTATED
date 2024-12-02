54c51ecdb282e35e48d182eb929d621a6336af43
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-02 09:23:57 -0600

    Update Office.h

--- Missions/Office.h
+++ Missions/Office.h
@@ -6,7 +6,7 @@
 //get characters used
 game::actor::import("Michael_Standard", "Tom_Standard_Suit");
 //add camera to scene
-game::component::addtoscene(game::component::CutsceneCamera());
+game::component::addtoscene(game::component::CutsceneCamera(0.0f, "Michael_Standard", -45.0f));
 //voice lines
 game::file::audio86("michael_intro_line.gav", "tom_intro_line.gav");
 
